5f637559cbf0ce6bf67fa16fa9729e3b574e6718
baileynottingham <baileynottingham@email.arizona.edu>
2017-04-23 15:23:49 -0700

    Update index.php
    minor changes

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!-- Homepage -->
-
+<!-- This whill contain all our products of images and buttons below them that we can add them to cart -->
 <html>
   <head>
     <title></title>
   <head>
   <body>
-      <h1>Banana </h1>
+      <h1>Welcome!</h1>
   </body>
 </html>

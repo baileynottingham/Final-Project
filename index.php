98e660173d6d72add4e53abe68efa9caaa961f38
baileynottingham <baileynottingham@email.arizona.edu>
2017-04-23 15:46:06 -0700

    Stylesheet
    Created a stylesheet

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!-- Homepage -->
 <!-- This whill contain all our products of images and buttons below them that we can add them to cart -->
+<!DOCTYPE html>
 <html>
   <head>
-    <title></title>
+    <title>Online Store</title>
+    <link href="styles.css" type="text/css" rel="stylesheet" />
   <head>
   <body>
-      <h1>Welcome!</h1>
-      <h1>Testing 1 2 3</h1>
+      <h1>(Online Store Name)</h1>
+      <hr />
+      <p> Copyright &copy; 2017 Bailey Nottingham and Scott Perry.</p>
   </body>
 </html>

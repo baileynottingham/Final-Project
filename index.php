a6b3f71b84970d6c0a82cb40528767e197510ef4
baileynottingham <baileynottingham@email.arizona.edu>
2017-04-23 20:43:55 -0700

    Database update
    Create a new account successfully gets added to database

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   if(isset($_POST["firstName"])) {
-    
+      require_once './Database.php';
+      $myDatabaseFunctions->addUser($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['password']);
       header("Location: index.php");
   }
   else {
